c7a34d4f81321a4bd2913658c2b25f4287ef32b0
Andy Hartford <andy.hartford@cohealo.com>
2016-06-08 16:38:09 -0400

    Refactor in to game and board classes

--- lib/main.py
+++ lib/main.py
@@ -5,7 +5,7 @@ def print_welcome(t):
     print(t.center(t.bold('Welcome to Connect 4. Press "q" or "Ctrl-c" to quit.')))
     print('')
 
-def render(t):
+def render(t, game):
     with t.location(0, 3):
         # todo move to specific location
         print('''
@@ -24,18 +24,77 @@ def render(t):
 .---.---.---.---.---.---.---.
         ''')
 
+
+    with t.location(0, 20):
+        print(game.message)
+
+class Board:
+    def __init__(self):
+        # array of 7 columns
+        self.board = [[], [], [], [], [], [], []]
+        self.message = ''
+
+    # Returns boolean whether move was successful. Check message on False.
+    def move(self, column_index, player):
+        try:
+            column = self.board[column_index]
+
+            if len(column) < 6:
+              column.append(player)
+              return True
+            else:
+              self.message = 'Column is full'
+              return False
+
+        except IndexError:
+            self.message = 'Column is unknown: {0}'.format(column_index)
+            return False
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.current_player = 'Red'
+        self.message = self.__default_message()
+
+    def move(self, column_index):
+        if self.board.move(column_index, self.current_player):
+            if self.current_player == 'Red':
+                self.current_player == 'Blk'
+            else:
+                self.current_player == 'Red'
+
+            self.message = self.__default_message()
+
+        else:
+            self.message = self.board.message
+
+    def __default_message(self):
+        return 'It is players {0} move. Press 0-6 to choose a column'.format(self.current_player)
+
 def main(t):
+    game = Game()
+
     with t.cbreak():
         while True:
-            render(t)
+            render(t, game)
             key = t.inkey()
             if key == 'q':
-              break
+                break
 
-            print(key)
+            try:
+                column_index = int(key)
+
+                if game.move(column_index):
+                    pass
+
+                else:
+                    pass
+
+            except ValueError:
+                pass
 
 term = Terminal()
+
 with term.fullscreen():
     print_welcome(term)
     main(term)
-

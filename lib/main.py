f304085ed52177ddc7fc76911229a38d38b7dfbf
Andy Hartford <andy.hartford@cohealo.com>
2016-06-08 15:52:35 -0400

    Draw board and allow quiting game

--- lib/main.py
+++ lib/main.py
@@ -1,1 +1,41 @@
-print('hello')
+from blessed import Terminal
+
+def print_welcome(t):
+    print('')
+    print(t.center(t.bold('Welcome to Connect 4. Press "q" or "Ctrl-c" to quit.')))
+    print('')
+
+def render(t):
+    with t.location(0, 3):
+        # todo move to specific location
+        print('''
+.---.---.---.---.---.---.---.
+|   |   |   |   |   |   |   |
+.---.---.---.---.---.---.---.
+|   |   |   |   |   |   |   |
+.---.---.---.---.---.---.---.
+|   |   |   |   |   |   |   |
+.---.---.---.---.---.---.---.
+|   |   |   |   |   |   |   |
+.---.---.---.---.---.---.---.
+|   |   |   |   |   |   |   |
+.---.---.---.---.---.---.---.
+|   |   |   |   |   |   |   |
+.---.---.---.---.---.---.---.
+        ''')
+
+def main(t):
+    with t.cbreak():
+        while True:
+            render(t)
+            key = t.inkey()
+            if key == 'q':
+              break
+
+            print(key)
+
+term = Terminal()
+with term.fullscreen():
+    print_welcome(term)
+    main(term)
+
